a9ef9673adb985a40ee4d2508b82dc9ec9420224
Ocomic <108867917+Ocomic@users.noreply.github.com>
2022-09-28 17:32:29 +0200

    small practicing
    only practicing windows forms stuff

--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,10 @@ namespace Hello_World_App
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
